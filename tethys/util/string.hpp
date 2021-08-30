24f66ed968197f06ff6c650ddf8c82dd7307c682
Emir Kotan <emirkotan@gmail.com>
2021-08-30 17:22:11 +0100

    string::kv uses sstream for doubles

--- tethys/util/string.hpp
+++ tethys/util/string.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <sstream>
 #include <string>
 
 namespace tethys::util::string {
+	std::string
+	from_double(double value)
+	{
+		std::ostringstream ss;
+		ss << value;
+		return ss.str();
+	}
+
 	template<class T> std::string kv(std::string key, T value);
 	template<> std::string kv(std::string key, const char* value);
+	template<> std::string kv(std::string key, double value);
 	template<> std::string kv(std::string key, std::string value);
 
 	template<class T>
@@ -19,6 +29,13 @@ namespace tethys::util::string {
 	kv(std::string key, const char* value)
 	{
 		return kv(key, std::string {value});
+	}
+
+	template<>
+	std::string
+	kv(std::string key, double value)
+	{
+		return kv(key, from_double(value));
 	}
 
 	template<>
